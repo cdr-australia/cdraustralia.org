7d931dd8bc8c4e2c518e688668c6dd36f7bfc9cb
Anup Kumar <154885652+cdr-australia@users.noreply.github.com>
2026-08-24 13:58:57 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'CDR Australia'
+copyright = '2026, CDR Australia'
+author = 'CDR Australia'
+release = '1.0'
+extensions = [
+    'myst_parser',
+]
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'sphinx_rtd_theme'
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
