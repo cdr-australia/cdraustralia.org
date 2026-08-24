1dce16ddde8b608f822db4dc1df959c4cd3b60d3
Anup Kumar <154885652+cdr-australia@users.noreply.github.com>
2026-08-24 13:40:58 +0530

    Delete docs/conf.py

--- docs/conf.py
+++
@@ -1,8 +0,0 @@
-project = 'CDR Australia'
-copyright = '2026, CDR Australia'
-author = 'CDR Australia'
-release = '1.0'
-extensions = []
-templates_path = ['_templates']
-exclude_patterns = []
-html_theme = 'sphinx_rtd_theme'
